d8dfc07b0eaf3de53cecc4f1e533e4bc06df3f4a
jlpython65 <105390022+jlpython65@users.noreply.github.com>
2022-05-12 11:08:13 -0400

    Update MADLIBSUPDATE.py

--- MADLIBSUPDATE.py
+++ MADLIBSUPDATE.py
@@ -14,13 +14,13 @@ def prompt(blank_list,story_list,story_index):
 
 
 story = "It was ___(FOOD)___ day at school, and ___(NAME)___ was super ___(ADJECTIVE)___ for lunch. But when she went outside to eat, a ___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the ___(NOUN)___ all over school. She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, ___(NAME)___’s friends were willing to share their ___(FOOD)___ with her."
-blanks = re.findall(r'___\(.+?\)___',story)
+blanks_list = re.findall(r'___\(.+?\)___',story)
 story_list = story.split()
 
 
-test1 = [i for i, s in enumerate(story_list, start=0) if re.search(r'___\(.+?\)___', s)] 
-for x in range(0,len(blanks)):
-    prompt(blanks[x],story_list,test1[x])
+story_index = [i for i, s in enumerate(story_list, start=0) if re.search(r'___\(.+?\)___', s)] 
+for x in range(0,len(blanks_list)):
+    prompt(blanks_list[x],story_list,story_index[x])
 
 seperator = " "
 new_story = seperator.join(story_list)
