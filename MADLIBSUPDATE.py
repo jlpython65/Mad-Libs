c70aa6fb75bac431994f3c7e861f371a4a2c6f1e
jlpython65 <105390022+jlpython65@users.noreply.github.com>
2022-05-12 11:05:57 -0400

    Update MADLIBSUPDATE.py

--- MADLIBSUPDATE.py
+++ MADLIBSUPDATE.py
@@ -7,21 +7,21 @@ from timeit import repeat
 from traceback import print_tb
 
 
-def prompt(sublist,storylist,storyindex):
-    global newword
-    newword = input("What's your word for {typeofword}".format(typeofword=sublist))
-    storylist[storyindex] = newword
+def prompt(blank_list,story_list,story_index):
+    global new_word
+    new_word = input("What's your word for {type_of_word}".format(type_of_word=blank_list))
+    story_list[story_index] = new_word
 
 
-string1 = "It was ___(FOOD)___ day at school, and ___(NAME)___ was super ___(ADJECTIVE)___ for lunch. But when she went outside to eat, a ___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the ___(NOUN)___ all over school. She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, ___(NAME)___’s friends were willing to share their ___(FOOD)___ with her."
-blanks = re.findall(r'___\(.+?\)___',string1)
-string1list = string1.split()
+story = "It was ___(FOOD)___ day at school, and ___(NAME)___ was super ___(ADJECTIVE)___ for lunch. But when she went outside to eat, a ___(NOUN)___ stole her ___(FOOD)___! ___(NAME)___ chased the ___(NOUN)___ all over school. She ___(VERB1)___, ___(VERB2)___, and ___(VERB3)___ through the playground. Then she tripped on her ___(NOUN)___ and the ___(NOUN)___ escaped! Luckily, ___(NAME)___’s friends were willing to share their ___(FOOD)___ with her."
+blanks = re.findall(r'___\(.+?\)___',story)
+story_list = story.split()
 
 
-test1 = [i for i, s in enumerate(string1list, start=0) if re.search(r'___\(.+?\)___', s)] 
+test1 = [i for i, s in enumerate(story_list, start=0) if re.search(r'___\(.+?\)___', s)] 
 for x in range(0,len(blanks)):
-    prompt(blanks[x],string1list,test1[x])
+    prompt(blanks[x],story_list,test1[x])
 
 seperator = " "
-newstory = seperator.join(string1list)
-print(newstory)
+new_story = seperator.join(story_list)
+print(new_story)
